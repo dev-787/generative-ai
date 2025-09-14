a4acbe8b6b8ad67e6e7862233d38563700947dd1
dev-787 <notexotic555@gmail.com>
2025-09-14 19:43:10 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AppRoutes from './AppRoutes.jsx'
+
+const App = () => {
+  return (
+    <>  
+      <AppRoutes />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/AppRoutes.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AppRoutes
