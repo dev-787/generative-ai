e58f859138e013bf1befca317c7aa1c855137223
dev-787 <notexotic555@gmail.com>
2025-09-14 20:37:38 +0530

    login register ui

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 import Home from './pages/Home.jsx'
 import Login from './pages/Login.jsx'
 import Register from './pages/Register.jsx'
+import Chatbot from './pages/Chatbot.jsx'
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Chatbot />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
